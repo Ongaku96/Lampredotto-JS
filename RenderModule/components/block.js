c274b84225af59909d4fbf2b971fb51c16903ad3
Ongaku96 <marco.biagini96@gmail.com>
2024-02-18 17:44:34 +0100

    updated components, playground

--- RenderModule/components/block.js
+++ RenderModule/components/block.js
@@ -7,18 +7,19 @@ defineComponent({
         flex-direction: row;
         justify-content: center;
         align-items: center;
+        border-radius: var(--global-radius);
 
         & > * {
-            flex-grow: auto;
-            border-radius: 0;
-            margin: 0;
+            flex-grow: 1;
+            border-radius: 0 !important;
+            margin: 0 !important;
 
             &:first-child {
-                border-radius: var(--global-radius) 0 0 var(--global-radius);
+                border-radius: var(--global-radius) 0 0 var(--global-radius) !important;
             }
 
             &:last-child {
-                border-radius: 0 var(--global-radius) var(--global-radius) 0;
+                border-radius: 0 var(--global-radius) var(--global-radius) 0 !important;
             }
         }
     }`);

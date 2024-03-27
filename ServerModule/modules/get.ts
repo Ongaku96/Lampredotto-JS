0f445a62f0aab0f514d17fd45822339eaf7e93c9
Ongaku96 <marco.biagini96@gmail.com>
2024-03-27 14:19:59 +0100

    ServerModule - update get

--- ServerModule/modules/get.ts
+++ ServerModule/modules/get.ts
@@ -1,17 +1,9 @@
-import { exception } from "./references.js";
 import { REST } from "./types.js";
 
 export class GetService extends REST {
 
     constructor(url: string) {
         super(url, "GET");
-    }
-
-    override async fetch() {
-        return await this.request().then((response) => {
-            if (!response.ok) throw exception(this, response);
-            return response;
-        });
     }
 
     async json(): Promise<object> {

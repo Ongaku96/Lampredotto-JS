d1f7275fd908007c5e5524be3909b3750783190f
Ongaku96 <marco.biagini96@gmail.com>
2024-08-09 11:30:57 +0200

    ServerModule
    exceptiona handler edited

--- ServerModule/modules/references.js
+++ ServerModule/modules/references.js
@@ -2,9 +2,13 @@ export const error_header = "LAMP Service: ";
 export const default_timer = 30000;
 export async function exception(target, response) {
     var error = await response.json();
-    if (error && error.ExceptionMessage)
-        console.error(`${error_header}${target.method} -> ${error.ExceptionMessage}`);
-    return new Error(response.statusText, { cause: error });
+    return new Error(error.ExceptionMessage ?? response.statusText, {
+        cause: {
+            target: target.method,
+            response: error,
+            message: `${error_header}${target.method} -> ${error.ExceptionMessage ?? response.statusText}`
+        }
+    });
 }
 /**Build url with search parameters created by dataset*/
 export function buildUrl(base, data) {

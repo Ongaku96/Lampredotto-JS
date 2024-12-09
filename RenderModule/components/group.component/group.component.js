88cfa801ba861378fc4d3e7a39b41428d09a97bd
Marco Biagini <52505540+Ongaku96@users.noreply.github.com>
2024-12-09 11:29:09 +0100

    groupcomponent style change

--- RenderModule/components/group.component/group.component.js
+++ RenderModule/components/group.component/group.component.js
@@ -2,7 +2,7 @@ import { defineComponent } from "../../LampRender.js";
 defineComponent({
     selector: "group-component",
     template: "<div group-component></div>",
-    styles: [`[group-component]{display:flex;flex-direction: row;justify-content: center;align-items: center;border-radius:var(--global-radius);}`,
+    styles: [`[group-component]{display:flex;flex-direction: row;justify-content: center;align-items: stretch;border-radius:var(--global-radius);}`,
         `[group-component]>*{flex-grow:1;border-radius:0 !important;margin:0 !important;}`,
         `[group-component]>*:first-child{border-radius:var(--global-radius) 0 0 var(--global-radius) !important;}`,
         `[group-component]>*:last-child{border-radius:0 var(--global-radius) var(--global-radius) 0 !important;}`,],
